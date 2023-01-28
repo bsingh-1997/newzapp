365c9ebafb7e0c95c095814b5f6c0c3a6e007627
barinder <barindersingh1997@gmail.com>
2023-01-28 12:43:42 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 import Navbar from './components/navbar';
 import News from './components/News';
 import Newsitem from './components/Newsitem';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
 function App() {
   return (
     <>
     <Navbar/>
-    <News pageSize={5}/>
+    
+    <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/newzapp' element={<News pageSize={5}/>} ></Route>
+          {/* <Route exact path="/about" element={<Middle />}></Route> */}
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   );
 }
